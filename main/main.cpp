b1da2d0652a4cf78a04ffab4d821a7abe347d96f
sQueezy1332 <47674362+sQueezy1332@users.noreply.github.com>
2025-12-15 10:27:41 +0300

    Update main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -3,9 +3,14 @@
 extern "C" void app_main() {
     main_init(); DEBUGLN(__TIMESTAMP__);
     ESP_LOGI(TAG, "Compiled: " __DATE__ "\t" __TIME__); 
+    Serial.println(ESP.getEfuseMac(), HEX);
     //read_noinit();  //0x253D7465 == crc8 //609862 //570586
     //ESP_LOGI(TAG,"%lu", ESP.getFreeHeap());nvs_test();ESP_LOGI(TAG,"%lu", ESP.getFreeHeap());
 #ifdef DEBUG_ENABLE
+    //generate_pin(12345678); //432130
+    {byte buf[64]; char str[64]; const int len = base32_decode(DEF_BLE_PASS, buf, sizeof(buf));
+    for (size_t i = 0; i < len; i++) { DEBUGF("0x%02X, ", buf[i]); };DEBUGF("\ncount = %d\n", len);
+    if(len > 0){base32_encode(buf, len, str, sizeof(str));DEBUGF("%s\nTOTP = %lu\n",str,TOTPget(buf, len, 12345678));}} //885100
     //pinMode(12, OUTPUT);
     //auto heart = esp_timer_init([](void*){ digitalToggle(12);}); esp_timer_start_periodic(heart, 1000*1000);
     //led_init();
@@ -16,12 +21,12 @@ extern "C" void app_main() {
     wifi_ap_init();
     wifi_server_init(); return;
 #else
-    gWrite(PIN_RELAY, 1); gWrite(PIN_LED, 1);//pinMode(PIN_RELAY, GPIO_MODE_INPUT_OUTPUT_OD);
+    dWrite(PIN_RELAY, 1); dWrite(PIN_LED, 1);//pinMode(PIN_RELAY, GPIO_MODE_INPUT_OUTPUT_OD);
    	gpio_config_t conf = {(BIT(PIN_RELAY) | BIT(PIN_LED)), GPIO_MODE_INPUT_OUTPUT_OD };
     gpio_config(&conf); gpio_set_drive_capability((gpio_num_t)PIN_RELAY,GPIO_DRIVE_CAP_0);
-    attachInterrupt(PIN_LINE, isr_handler, GPIO_INTR_ANYEDGE); 
+    attachInterrupt(PIN_LINE, isr_handler, GPIO_INTR_ANYEDGE);
     nvs_read_sets();
-    read_auth_data(); log_d("password_len %u, scan_key_len: %u\n", password_len, scan_key_len);DEBUGLN(password);
+    read_auth_data(); log_d("passkey_len %u, scan_key_len: %u\n", passkey_len, scan_key_len);//DEBUGLN(passkey);
     read_bonded_mac();
     ESP_ERROR_CHECK(nimble_port_init());
     gap_init();
@@ -40,8 +45,7 @@ extern "C" void app_main() {
 
 static void mainTask(void *) {
     for(;;) {
-        __unused uint32_t notify = ulTaskNotifyTake(0,portMAX_DELAY);
-        //switch () { 
+        switch (ulTaskNotifyTake(1,portMAX_DELAY)) { 
         //case OTA:
             //vTaskSuspend(ble_handle);
             //set_boot_partition(ESP_PARTITION_SUBTYPE_APP_FACTORY);
@@ -49,22 +53,23 @@ static void mainTask(void *) {
 		//case RESTART: vTaskDelay(1); esp_restart(); break;
         //case NOTIFY: send_alarm_notify(); break;
         //case VALID: esp_ota_mark_app_valid_cancel_rollback(); break;
-		//} 
-        if(need_notify()) send_alarm_notify();
+        case NOTIFY_ALARM: send_alarm_notify(); break;
+        case NOTIFY_TIME: break;
+		} 
     }
 }
 
 static void isr_handler() {
-    static bool prev_state = 1;
-    const bool now = gRead(PIN_LINE); ESP_DRAM_LOGI("ISR", "%u", now);
+    static int prev_state = 1;
+    const int now = dRead(PIN_LINE); ESP_DRAM_LOGI("ISR", "%u", now);
     if(prev_state != now) {
         prev_state = now;
         if(sets.patch == false) { 
-            gWrite(PIN_RELAY, now); 
+            dWrite(PIN_RELAY, now); 
         }
-        else if(need_notify()) { xTaskNotifyFromISR(main_handle, 0, eNoAction, NULL); };
+        else if(need_notify()) { xTaskNotifyFromISR(main_handle, NOTIFY_ALARM, eNoAction, NULL); };
 //#ifdef DEBUG_ENABLE
-        gWrite(PIN_LED, now);
+        dWrite(PIN_LED, now);
 //#endif
     }
 }
@@ -92,41 +97,6 @@ void rand_device_name() {
     pName[name_len] = ' ';
     const uint32_t rand_num = esp_random(); log_d("%lu",rand_num);
     bytes_to_str<false, 0>(&pName[name_len + 1], (byte*)&rand_num, sizeof(rand_num));
-}
-
-uint32_t generate_pin(uint32_t salt, const char *pass, byte pass_len) {
-    cbyte salt_len = sizeof(salt)*2/* strlen(str) */
-    , payloadLen = salt_len + pass_len;
-    byte shaResult[20]; byte payload[payloadLen];
-    mbedtls_md_context_t ctx {};
-    bytes_to_str<false, 0>((char*)payload,(byte*)&salt, 4);
-    //memcpy(payload, &salt, salt_len);
-    memcpy(&payload[salt_len], pass, pass_len);
-    DEBUGLN((char*)payload); DEBUGF("salt_len = %u; ""payloadLen = %u\n", salt_len, payloadLen);
-    mbedtls_md_setup(&ctx, mbedtls_md_info_from_type(MBEDTLS_MD_SHA1), 0);
-    mbedtls_md_starts(&ctx);
-    mbedtls_md_update(&ctx, payload,  payloadLen);
-    mbedtls_md_finish(&ctx, shaResult);
-    mbedtls_md_free(&ctx);
-    DEBUG("Hash: "); for (byte i = 0; i < sizeof(shaResult); i++) { DEBUGF("%02x", shaResult[i]);} DEBUGLN();
-    uint32_t crcResult = crc32_le(0, shaResult, sizeof(shaResult));
-    DEBUGF("crcResult = %lu\n", crcResult);
-    crcResult = 1000 + crcResult % 999000;
-    DEBUGF("pin = %lu\n", crcResult);// return 100000 + esp_random() % 900000,
-    return crcResult;
-}
-
-uint32_t generate_salt() {
-    static uint32_t last_change = __INT32_MAX__, salt; //ESP_LOGD(TAG, "change_device_name()");
-    uint32_t sec = xTaskGetTickCount();
-    if(sec - last_change > pdMS_TO_TICKS(TIME_CHANGE_PIN)) { ESP_LOGI(TAG, "TIME_CHANGE_PIN");
-        last_change = sec;
-#if !_ESP_LOG_ENABLED(3)
-        ble_delete_all_peers(&bonded_addr);
-#endif
-        pincode = generate_pin(salt = esp_random());
-    }
-    return salt;
 }
 
 void ble_delete_all_peers(bond_mac_s* except) {
@@ -187,22 +157,22 @@ void read_auth_data() {
     nvsApi handle; size_t required_size;
     CHECK_VOID(handle.begin(NVS_SPACE_SETS, NVS_READONLY));
     CHECK_VOID(nvs_get_blob(handle, NVS_KEY_BLE_PASS, NULL, &required_size));
-    if(required_size < 16 || required_size > sizeof(password)) return;
-    CHECK_VOID(nvs_get_blob(handle, NVS_KEY_BLE_PASS, password, &required_size));
-    password_len = required_size;
+    if(required_size < 16 || required_size > sizeof(passkey)) return;
+    CHECK_VOID(nvs_get_blob(handle, NVS_KEY_BLE_PASS, passkey, &required_size));
+    passkey_len = required_size;
     CHECK_VOID(nvs_get_blob(handle, NVS_KEY_SCAN_DATA, NULL, &required_size));
     if(required_size < 8 || required_size > sizeof(scan_key)) return;
     CHECK_VOID(nvs_get_blob(handle, NVS_KEY_SCAN_DATA, scan_key, &required_size));
     scan_key_len = required_size;
 }
 
-esp_err_t save_auth_data(cch* pass) {
+esp_err_t save_auth_data() {
     //extern auth_t Auth;
-    if (!pass && !scan_key_len) { ESP_LOGW(TAG, "!Auth"); return 0xFFFF; }
+    if (!passkey_len && !scan_key_len) { ESP_LOGW(TAG, "!Auth"); return 0xFFFF; }
     nvsApi handle;
     CHECK_RET(handle.begin(NVS_SPACE_SETS, NVS_READWRITE));
-    if(pass && password_len) {
-        CHECK_RET(nvs_set_blob(handle, NVS_KEY_BLE_PASS, pass, password_len));
+    if(passkey_len) {
+        CHECK_RET(nvs_set_blob(handle, NVS_KEY_BLE_PASS, passkey, passkey_len));
     }
     if(scan_key_len) {
         CHECK_RET(nvs_set_blob(handle, NVS_KEY_SCAN_DATA, scan_key, scan_key_len));
@@ -252,7 +222,8 @@ void parse_rx_data(const os_mbuf *buf) {
 		esp_ota_mark_app_valid_cancel_rollback(); break;
     //case 1234: set_cts_unix(val);
     case DEF_DELETE_ALL_PEERS: ble_delete_all_peers(); break;
-    case DEF_NVS_ERASE_ALL: nvsErase(); break;
+    case DEF_NVS_ERASE_ALL: nvsErase("phy"); break;
+    case DEF_NVS_ERASE_ALL + 1: nvsErase(nullptr); break;
     case DEF_SAVE_MAC:  save_bonded_mac(desc.peer_id_addr); break;
     case DEF_PRINT_KEY: DEBUG(get_task_list()); break;
     default: ESP_LOGW("key", "0x%08X", val);
@@ -319,6 +290,134 @@ rdy:            buf[i] = result;
     data_size = i;
 	//return realloc(buf, i);
     //for (size_t j = 0; j < i; j++){ DEBUGF("%02X ", buf[j]); }DEBUGLN();
+}
+
+void set_ble_device_name() {
+    const size_t name_len = sizeof(MYNEWT_VAL(BLE_SVC_GAP_DEVICE_NAME))-1; static_assert(name_len >= 7);
+    char* name = const_cast<char*>(ble_svc_gap_device_name()); byte mac[8];
+    *(name += name_len) = '-';
+    CHECK_VOID(esp_read_mac(mac, ESP_MAC_BT));
+    bytes_to_str<true, 0>(name+1, mac + 3, 3);
+}
+
+uint32_t generate_salt() {
+    static uint32_t last_change = __INT32_MAX__, salt; //ESP_LOGD(TAG, "change_device_name()");
+    uint32_t sec = xTaskGetTickCount();
+    if(sec - last_change > pdMS_TO_TICKS(TIME_CHANGE_PIN)) { 
+        last_change = sec;
+#if !_ESP_LOG_ENABLED(3)
+        ble_delete_all_peers(&bonded_addr);
+#endif
+        salt = time(NULL); 
+        if(salt < 1765832400) salt = 0;
+        pincode = TOTPget(passkey, passkey_len, salt); ESP_LOGI(TAG, "NEW PIN: %lu\nTime: %lu", pincode, salt);
+    }
+    return salt;
+}
+
+uint32_t TOTPget(const byte* key, byte key_len, uint32_t time) {
+    return HOTPget(key, key_len, time / TOTP_TIMESTEP);
+}
+
+uint32_t HOTPget(const byte* key, byte key_len, uint64_t salt) {
+    const byte salt_len = sizeof(salt); byte* const pSalt = reinterpret_cast<byte*>(&salt);
+    byte hash[20];  uint32_t result;
+
+    swap_in_place(pSalt, salt_len); //salt = ntohll(salt);
+
+    int ret = hmac_hash(hash, key, key_len, pSalt, salt_len); 
+    if(ret != 0) return 0;
+
+	for (byte i = 0, offset = hash[19] & 0xF; i < 4; ++i) {
+		reinterpret_cast<byte*>(&result)[3 - i] = hash[offset + i];
+	}
+
+	result = (result & 0x7FFFFFFF) % 1000000;
+	return result;
+}
+
+int hmac_hash(byte* hash, const byte* key, size_t key_len, const byte* data, size_t data_len, mbedtls_md_type_t t) {
+    mbedtls_md_context_t ctx {/*init*/};
+    CHECK_RET(mbedtls_md_setup(&ctx, mbedtls_md_info_from_type(t), 1));
+    CHECK_RET(mbedtls_md_hmac_starts(&ctx, key, key_len));
+    CHECK_RET(mbedtls_md_hmac_update(&ctx, data,  data_len));
+    CHECK_RET(mbedtls_md_hmac_finish(&ctx, hash));
+    mbedtls_md_free(&ctx);
+    return 0;
+}
+
+/**
+ * Base32 decoder
+ * From https://github.com/google/google-authenticator-libpam/blob/master/src/base32.c
+ * @param encoded Encoded text
+ * @param result Bytes output
+ * @param buf_len Bytes length
+ * @return -1 if failed, or length decoded
+ */
+int base32_decode(const char* encoded, uint8_t* result, size_t buf_len) {
+    if (!encoded || !result) {
+        ESP_LOGE(TAG, "String or buffer are null"); return -1;
+    }
+    // Base32's overhead must be at least 1.4x than the decoded bytes, so the result output must be bigger than this
+    /* size_t expect_len = ceil(strlen(encoded) / 1.6);
+    if (buf_len < expect_len) {
+        ESP_LOGE(TAG, "Buffer length is too short, only %u, need %u", buf_len, expect_len);
+        return -1;
+    } */
+    int buffer = 0, bits_left = 0, count = 0; 
+    for (const char* ptr = encoded; count < buf_len && *ptr; ++ptr) {
+        char ch = *ptr;
+        buffer <<= 5;
+        // Deal with commonly mistyped characters
+        switch (ch) {
+            case ' ': case '\t':case '\r':case '\n': case '-': continue;
+            case '=': return count;
+            /* case '0': ch = 'O';break;
+            case '1': ch = 'L';break;
+            case '8': ch = 'B';break; */
+        }
+            // Look up one base32 digit
+        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+            ch = (ch & 0b11111) - 1;
+        } else if (ch >= '2' && ch <= '7') {
+            ch -= '2' - 26;
+        } else {
+            ESP_LOGE(TAG, "Invalid Base32!");
+            return -1;
+        }
+        buffer |= ch;
+        bits_left += 5;
+        if (bits_left >= 8) {
+            result[count++] = buffer >> (bits_left - 8);
+            bits_left -= 8;
+        }
+    }
+    if (count < buf_len) { result[count] = '\000';} 
+    return count;
+}
+
+int base32_encode(const uint8_t *data, size_t length, char *result, size_t encode_len) {
+    if (!length ||  length > (1 << 16)) { return -1; } //28
+    int count = 0;
+        int buffer = data[0], next = 1, bits_left = 8;
+        while (count < encode_len && (bits_left > 0 || next < length)) {
+            if (bits_left < 5) {
+                if (next < length) {
+                    buffer <<= 8;
+                    buffer |= data[next++] & 0xFF;
+                    bits_left += 8;
+                } else {
+                    int pad = 5 - bits_left;
+                    buffer <<= pad;
+                    bits_left += pad;
+                }
+            }
+            int index = 0x1F & (buffer >> (bits_left - 5));
+            bits_left -= 5;
+            result[count++] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567"[index];
+        }
+        if (count < encode_len) { result[count] = '\0'; }
+    return count;
 }
 
 void read_noinit() {
