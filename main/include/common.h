032e78bb6530b9dd8e8d2356786863473de8135b
sQueezy1332 <47674362+sQueezy1332@users.noreply.github.com>
2026-04-30 18:51:04 +0300

    Update common.h

--- main/include/common.h
+++ main/include/common.h
@@ -6,21 +6,29 @@
 //#include <string.h>
 
 /* ESP APIs */
-#include "esp_log.h"
 //#include "esp_random.h"
 //#include "nvs_flash.h"
-//#include "sdkconfig.h"
 #include "esp_check.h"
 
 /* FreeRTOS APIs */
 //#include <freertos/FreeRTOS.h>
 //#include <freertos/task.h>
-
+#define _WANT_USE_LONG_TIME_T
 /* NimBLE stack APIs */
 #include "host/ble_hs.h"
 #include "host/ble_uuid.h"
 #include "host/util/util.h"
-#include "nimble/ble.h"
-#include "nimble/nimble_port.h" 
-#include "nimble/nimble_port_freertos.h" 
-#include "nimble/nimble_port.h"
+//#include "nimble/ble.h"
+
+#if _ESP_LOG_ENABLED(3)
+#define NIMLOG(msg, ...) esp_rom_printf((msg), ##__VA_ARGS__)
+#define SCAN_PASSIVE 0 
+#else
+#define NIMLOG(msg, ...) 
+#define SCAN_PASSIVE 1
+#endif
+
+#define FUNC_ADDRESS (esp_cpu_get_call_addr((intptr_t)__builtin_return_address(0)))
+#define CHECK_(x) ESP_ERROR_CHECK_WITHOUT_ABORT(x)
+#define CHECK_RET(x) ESP_RETURN_ON_ERROR(x,"","0x%08x",FUNC_ADDRESS)
+#define CHECK_VOID(x) ESP_RETURN_VOID_ON_ERROR(x,"","0x%08x",FUNC_ADDRESS)
