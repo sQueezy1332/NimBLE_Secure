327d16cabbb89d826935efd24966c5ceca81bd8e
sQueezy1332 <47674362+sQueezy1332@users.noreply.github.com>
2026-05-13 17:54:19 +0300

    Update wifi_api.h

--- main/include/wifi_api.h
+++ main/include/wifi_api.h
@@ -1,11 +1,15 @@
 #pragma once
+#define _WANT_USE_LONG_TIME_T
 #include "esp_netif.h"
 #include "esp_wifi.h"
 #include "lwip/err.h"
 #include "lwip/sys.h"
 #include "esp_mac.h"
 #include "esp_log.h"
-#define _WANT_USE_LONG_TIME_T
+
+#define STATION_MODE
+#define ACCESS_POINT_MODE
+
 #define ALIGN_TO_16(size) (((size) + 15) & ~15)
 
 #define DHCPS_OFFER_DNS			0x02
@@ -14,10 +18,16 @@
 #define WIFI_STA_GOT_IP 		BIT2
 #define WIFI_AP_STACONNECTED 	BIT3
 #define WIFI_AP_STAIPASSIGNED	BIT4
+#define WIFI_REASON_HANDSHAKE_TOUT	BIT5
+
 
 //struct EventGroupHandle_t; //struct esp_netif_t;
 extern EventGroupHandle_t h_group_wifi;
 extern esp_event_handler_instance_t h_event_wifi, h_event_ip;
+
+extern void wifi_timer_stop();
+
+extern void wifi_timer_start();
 
 void wifi_setup_default(wifi_mode_t, wifi_storage_t = WIFI_STORAGE_FLASH);
 
@@ -27,8 +37,10 @@ esp_netif_t* wifi_init_ap();
 
 void ap_set_dns_addr(esp_netif_t *ap,esp_netif_t *sta);
 
-void print_mac(char* buf, const uint8_t (&mac)[6]);
+const char* print_mac(const uint8_t (&mac)[6]);
 
-void print_ip(char* buf, uint32_t addr);
+const char* print_ip(uint32_t addr);
+
+int wifi_is_connected();
 
 int wifi_ap_get_sta_num();
