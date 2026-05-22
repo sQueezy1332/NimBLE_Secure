c94161ffa9521b59600e7530a84d7a99597ff8c2
sQueezy1332 <47674362+sQueezy1332@users.noreply.github.com>
2026-05-23 02:00:47 +0300

    Update gap.h

--- main/include/gap.h
+++ main/include/gap.h
@@ -6,9 +6,8 @@
  */
 
 /* NimBLE GAP APIs */
-/* #include "host/ble_gap.h"
-#include "services/gap/ble_svc_gap.h" */
-
+//#include "host/ble_gap.h"
+//#include "services/gap/ble_svc_gap.h"
 /* Defines */
 #define DEVICE_NAME CONFIG_IDF_TARGET //CONFIG_BT_NIMBLE_SVC_GAP_DEVICE_NAME
 #define DEVICE_NAME_LEN (sizeof(DEVICE_NAME)-1) 
@@ -31,10 +30,10 @@
 extern "C" {
 #endif
 //static int ble_gap_callback_event(struct ble_gap_event *event, void *arg);
-void ble_store_config_init(void);
+extern void ble_store_config_init(void);
 const char* ble_svc_gap_device_name(void);
 int ble_svc_gap_device_name_set(const char *);
-void ble_store_config_conf_init();
+void ble_store_config_conf_init(); //CONFIG_BT_NIMBLE_NVS_PERSIST=y
 struct ble_hs_adv_fields; struct ble_gap_conn_desc; struct ble_hs_cfg; 
 struct ble_gap_event; struct os_mbuf; struct ble_gatt_register_ctxt;
 union ble_store_key; union ble_store_value; 
@@ -45,7 +44,8 @@ union ble_store_key; union ble_store_value;
 void ble_hs_cfg_init();
 int gap_init();
 void host_sync_cb();
-void ble_scan_adv();
+
+void ble_scan_init();
 void adv_init();
 int save_bonding(uint16_t conn_handle);
 
@@ -54,8 +54,8 @@ extern "C" {
 #endif
 bool is_connection_encrypted(uint16_t);
 int gatt_svr_subscribe_cb(const ble_gap_event *);
-void gatt_svr_register_cb(struct ble_gatt_register_ctxt *ctxt, void *arg);
-void clear_characteristic();
+void gatt_svr_register_cb(struct ble_gatt_register_ctxt *, void *);
+void clear_characteristic(size_t);
 void set_encryption();
 void impl_io_on();
 void impl_io_off();
@@ -69,5 +69,4 @@ extern uint32_t generate_salt();
 extern void print_task_list();
 extern void set_ble_device_name();
 extern void unpatch_cb();
-//extern void start_adv();
-extern void parse_rx_data(const ble_gap_event* event);
+extern void parse_rx_data(const ble_gap_event *);
