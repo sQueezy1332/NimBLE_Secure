5a8781885edb93afce7edc7dfa0e3a39d27e18a5
sQueezy1332 <47674362+sQueezy1332@users.noreply.github.com>
2026-05-25 14:40:18 +0300

    Update common.h

--- main/include/common.h
+++ main/include/common.h
@@ -22,7 +22,7 @@
 //#include "nimble/ble.h"
 #include "modlog/modlog.h"
 
-#if _ESP_LOG_ENABLED(3)
+#if _ESP_LOG_ENABLED(5)
 #define NIMLOG(msg, ...) esp_rom_printf((msg), ##__VA_ARGS__)
 #define SCAN_PASSIVE 0 
 #else
@@ -32,5 +32,5 @@
 
 #define FUNC_ADDRESS (esp_cpu_get_call_addr((intptr_t)__builtin_return_address(0)))
 #define CHECK_(x) ESP_ERROR_CHECK_WITHOUT_ABORT(x)
-#define CHECK_RET(x) ESP_RETURN_ON_ERROR(x,"","0x%08x",FUNC_ADDRESS)
-#define CHECK_VOID(x) ESP_RETURN_VOID_ON_ERROR(x,"","0x%08x",FUNC_ADDRESS)
+#define CHECK_RET(x) ESP_RETURN_ON_ERROR(x,"","err 0x%02x at 0x%08x", x, FUNC_ADDRESS)
+#define CHECK_VOID(x) ESP_RETURN_VOID_ON_ERROR(x,"","err 0x%02x at 0x%08x", x, FUNC_ADDRESS)
