b81351f2a7d414a496a5caeb9c485900219b76bc
sQueezy1332 <47674362+sQueezy1332@users.noreply.github.com>
2026-09-12 10:00:35 +0300

    Update ESP_MAIN.h

--- main/libs/misc/ESP_MAIN.h
+++ main/libs/misc/ESP_MAIN.h
@@ -122,3 +122,95 @@ public:
 	~nvsApi() { close(); ESP_LOGV("NVS", "~_handle = %lu", _handle); };
 	operator nvs_handle_t() const { return _handle; };
 };
+
+#include "esp_partition.h"
+inline void partition_read() {
+	auto i = esp_partition_find(ESP_PARTITION_TYPE_ANY, ESP_PARTITION_SUBTYPE_ANY, NULL);
+	for (;i; i = esp_partition_next(i)) {
+		const esp_partition_t* partArr = esp_partition_get(i);
+		ESP_LOGI("partition", "Label %s, size %lu, address 0x%lX", 
+			partArr->label, partArr->size, partArr->address);
+	}
+	esp_partition_iterator_release(i);
+}
+
+inline bool nvsOpen(const char* name_group, nvs_open_mode_t open_mode, nvs_handle_t* nvs_handle) {
+	esp_err_t err = nvs_open(name_group, open_mode, nvs_handle);
+	if (err == ESP_OK) return true;
+	if (err != ESP_ERR_NVS_NOT_FOUND || open_mode != NVS_READONLY) {
+		ESP_LOGE("NVS", "Error opening NVS namespace \"%s\": %d (%s)!", name_group, err, esp_err_to_name(err));
+	}
+	return false;
+}
+
+inline esp_err_t nvsGet(nvs_handle_t handle, cch* key, nvs_type_t type, void* &buf_out, size_t* size_out = nullptr) {
+	esp_err_t ret; size_t required_size; void* ptr; *(uint32_t*)buf_out = 0x0;
+	switch (type) {
+	case NVS_TYPE_U8:ret = nvs_get_u8(handle, key, (uint8_t*)buf_out); break;
+	case NVS_TYPE_I8:ret = nvs_get_i8(handle, key, (int8_t*)buf_out); break;
+	case NVS_TYPE_U16:ret = nvs_get_u16(handle, key, (uint16_t*)buf_out); break;
+	case NVS_TYPE_I16:ret = nvs_get_i16(handle, key, (int16_t*)buf_out); break;
+	case NVS_TYPE_U32:ret = nvs_get_u32(handle, key, (uint32_t*)buf_out); break;
+	case NVS_TYPE_I32:ret = nvs_get_i32(handle, key, (int32_t*)buf_out); break;
+	case NVS_TYPE_U64:ret = nvs_get_u64(handle, key, (uint64_t*)buf_out);
+		if(ret) return ret;
+		ret = 1; break;
+	case NVS_TYPE_I64:ret = nvs_get_i64(handle, key, (int64_t*)buf_out);
+		if(ret) return ret;
+		return 1;
+	case NVS_TYPE_STR:
+		if((ret = nvs_get_str(handle, key, NULL, &required_size))) return ret;
+		ptr = realloc(buf_out, required_size);
+		if (!ptr) return ESP_ERR_NO_MEM; buf_out = ptr;
+		nvs_get_str(handle, key, (char*)buf_out, &required_size);
+		if (size_out) *size_out = required_size; 
+		return 2;
+	case NVS_TYPE_BLOB: 
+		if((ret = nvs_get_blob(handle, key, NULL, &required_size))) return ret;
+		ptr = realloc(buf_out, required_size);
+		if (!ptr) return ESP_ERR_NO_MEM; buf_out = ptr;
+		nvs_get_blob(handle, key, buf_out, &required_size);
+		if (size_out) *size_out = required_size;
+		return 3;
+		default: return ESP_FAIL;
+	}
+	return ret;
+}
+
+inline void nvs_test(const char* key = nullptr) {
+	__unused static const char* TAG = "NVS";
+	esp_err_t ret; void* buf = malloc(64); size_t buf_len = 0;
+	nvs_stats_t nvs_stats {}; nvs_entry_info_t entry; nvs_iterator_t it = NULL;
+	nvs_get_stats(NULL, &nvs_stats);
+	ESP_LOGI(TAG, "Used %u, Free %u, Available %u, All = %u, Namespaces %u entries",
+		nvs_stats.used_entries, nvs_stats.free_entries, nvs_stats.available_entries, nvs_stats.total_entries, nvs_stats.namespace_count);
+	for (ret = nvs_entry_find("nvs", NULL, NVS_TYPE_ANY, &it); ret == ESP_OK; ret = nvs_entry_next(&it)) {
+		nvs_entry_info(it, &entry); // Can omit error check if parameters are guaranteed to be non-NULL
+		nvsApi nvs; ESP_LOGI(TAG, "space '%s'\tkey '%s' type '%u'", entry.namespace_name, entry.key, entry.type);
+		if(key && !strcmp(key,entry.key)) continue;
+		if(!nvs.begin(entry.namespace_name, NVS_READONLY)) {
+			switch (esp_err_t ret = nvsGet(nvs, entry.key, entry.type, buf, &buf_len)) {
+			case ESP_OK: 
+				DEBUGF("Data = %lu\n", *(uint32_t*)buf); 
+				break;
+			case 1:
+				#ifdef CONFIG_LIBC_NEWLIB_NANO_FORMAT
+				DEBUGF("Data = "); if(((uint32_t*)buf)[1]) { DEBUGF("%02lX", ((uint32_t*)buf)[1]); }
+				DEBUGF("%08lX", ((uint32_t*)buf)[0]); DEBUGLN();
+				#else
+				DEBUGF("Data = %llu\n", *(uint64_t*)buf);
+				#endif
+				break;
+			case 2: 
+				ESP_LOGI(TAG, "Str: %s\n", (char*)buf); break;
+			case 3: 
+				ESP_LOGI(TAG, "Blob size %u: ", buf_len);
+				for (size_t i = 0; i < buf_len; i++) { DEBUGF("%02X ", ((uint8_t*)buf)[i]); }; DEBUGLN();
+				break;
+			default: ESP_LOGE(TAG, "nvsGet()" " 0x%X", ret);
+			} DEBUGF("\n");
+		}
+	}
+	free(buf);
+	nvs_release_iterator(it);
+}
